37d08d31a36c7680e1b42a59b4d849294f0cc205
Nicolás Sebastián Estigarribia <nicolas.sebastian.estigarribia@gmail.com>
2023-05-16 17:21:35 -0300

    Add project files.

---
+++ UiDesktopApp1/ViewModels/CargasViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Windows.Input;
+using Wpf.Ui.Common.Interfaces;
+
+namespace UiDesktopApp1.ViewModels
+{
+    public partial class CargasViewModel : ObservableObject, INavigationAware
+    {
+        private bool _isInitialized = false;
+        public void OnNavigatedTo()
+        {
+            if (!_isInitialized)
+                InitializeViewModel();
+        }
+
+        public void OnNavigatedFrom()
+        {
+        }
+
+        public void InitializeViewModel()
+        {
+        }
+
+    }
+}
---
+++ UiDesktopApp1/Views/Pages/CargasPages.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Wpf.Ui.Common.Interfaces;
+
+namespace UiDesktopApp1.Views.Pages
+{
+    /// <summary>
+    /// Interaction logic for SettingsPage.xaml
+    /// </summary>
+    public partial class CargasPages : INavigableView<ViewModels.CargasViewModel>
+    {
+        public ViewModels.CargasViewModel ViewModel
+        {
+            get;
+        }
+
+        public CargasPages(ViewModels.CargasViewModel viewModel)
+        {
+            ViewModel = viewModel;
+
+            InitializeComponent();
+        }
+    }
+}
